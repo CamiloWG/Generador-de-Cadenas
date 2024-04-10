c5f908116a0a8222b8ec56437ab1f3cc2f6ca435
Sergio Camilo Madero Baldión <smadero@wi-mobile.com>
2024-04-10 10:34:50 -0500

    First commit

---
+++ GeneradorCadenas/src/main/java/com/mycompany/generadorcadenas/GeneradorCadenas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.generadorcadenas;
+
+/**
+ *
+ * @author smadero
+ */
+public class GeneradorCadenas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
